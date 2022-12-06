0a6aec10ded81190198c682372855b88c404949e
Oksana <ksanka82@yandex.ru>
2022-12-06 11:52:14 +0300

    Task34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -2,7 +2,7 @@
 {
     int[] array = new int[size];
     for (int i = 0; i < size; i++)
-        array[i] = new Random().Next(1, 100);
+        array[i] = new Random().Next(100, 1000);
     return array;
 }
 

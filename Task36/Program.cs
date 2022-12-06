fd38b846d88e7e6ae820feb3c86aea73fb0cd024
Oksana <ksanka82@yandex.ru>
2022-12-06 10:09:33 +0300

    Задача36

---
+++ Task36/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(-10, 10);
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int el in array)
+        Console.Write($"{el} ");
+    Console.WriteLine();
+}
+int sum = 0;
+int Sum(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+       if(i%2==1)
+       sum=sum+array[i];
+    }
+    return sum;
+}
+Console.WriteLine("Введите размерность массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(size);
+PrintArray(array);
+Console.WriteLine(Sum(array));
